655620fb3b069b34e80f3f1dc8dafd9984b79d57
jvadebossan <jvdebosssan@gmail.com>
2022-01-06 16:40:18 -0300

    fix app

--- app.py
+++ app.py
@@ -13,19 +13,22 @@ CORS(app)
 
 @app.route('/api/home', methods=['GET'])
 def staff():
-    obj = {}
-    result = site.find_one({"_id": 1})
-    obj['staff'] = []
-    obj['staff'].append(result['dono'])
-    for i in ['admins', 'mods']:
-        for e in result[i]:
-            obj['staff'].append(e)
-    obj['booster'] = result['boosters']
-    obj['info'] = {}
-    obj['info']['channel_count'] = (result['channel_count'])
-    obj['info']['member_count'] = (result['member_count'])
-    obj['info']['staff_count'] = (len(obj['staff']))
-    return obj
+    try:
+        obj = {}
+        result = site.find_one({"_id": 1})
+        obj['staff'] = []
+        obj['staff'].append(result['dono'])
+        for i in ['admins', 'mods']:
+            for e in result[i]:
+                obj['staff'].append(e)
+        obj['booster'] = result['boosters']
+        obj['info'] = {}
+        obj['info']['channel_count'] = (result['channel_count'])
+        obj['info']['member_count'] = (result['member_count'])
+        obj['info']['staff_count'] = (len(obj['staff']))
+        return obj
+    except:
+        return '<h1>Deu erro ae pae</h1>'
 
 @app.route('/api/ranking')
 def ranking():
