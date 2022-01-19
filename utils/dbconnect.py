b47c66fc4bf211f09d6b5b91c3e3e3ff822f30d5
jvadebossan <jvdebosssan@gmail.com>
2022-01-19 03:36:01 -0300

    fix env

--- utils/dbconnect.py
+++ utils/dbconnect.py
@@ -1,7 +1,7 @@
 import os
-from boto.s3.connection import S3Connection
+
 from pymongo import MongoClient
 
 def mongoConnect():
-    uri = S3Connection(os.environ['MONGO_URI'])
+    uri = os.environ.get('MONGO_URI', None)
     return MongoClient(uri)
